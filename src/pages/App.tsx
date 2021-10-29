ccbd2cf206da9035a8b14892d46ec42da1dbe4ae
tunogya <tunogya@qq.com>
2021-10-29 16:37:40 +0800

    delete Stack

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,4 +1,4 @@
-import {color, Spacer, Stack, Tab, TabList, TabPanel, TabPanels, Tabs, Text} from "@chakra-ui/react"
+import {Spacer, Stack, Tab, TabList, TabPanel, TabPanels, Tabs, Text} from "@chakra-ui/react"
 import Futures from "./Futures"
 import Options from "./Options"
 import Users from "./Users"
@@ -6,31 +6,31 @@ import logo from "../assets/svg/logo.svg";
 
 const App = () => {
   return (
-    <Stack alignItems={"center"}>
-      <Tabs isLazy>
-        <TabList alignItems={"center"}>
-          <Stack pl={["22px", "22px", "44px"]}>
-            <img src={logo}/>
-          </Stack>
-          <Spacer/>
-          <Tab h={"66px"} fontWeight={"bold"}>Futures</Tab>
-          <Tab h={"66px"} fontWeight={"bold"}>Options</Tab>
-          <Tab h={"66px"} fontWeight={"bold"}>User</Tab>
-          <Spacer/>
-        </TabList>
-        <TabPanels>
-          <TabPanel p={0}>
-            <Futures/>
-          </TabPanel>
-          <TabPanel p={0}>
-            <Options/>
-          </TabPanel>
-          <TabPanel p={0}>
-            <Users/>
-          </TabPanel>
-        </TabPanels>
-      </Tabs>
-    </Stack>
+    <Tabs isLazy>
+      <TabList alignItems={"center"}>
+        <Stack pl={["22px", "22px", "44px"]} direction={"row"} spacing={4} position={"absolute"}>
+          <img src={logo} alt={"logo"}/>
+          <Text px={4} py={2} borderRadius={"20px"} boxShadow={"0 0 10px #E5E5E5"} fontWeight={600}
+                fontSize={"18px"} color={"#878787"}>Dashboard</Text>
+        </Stack>
+        <Spacer/>
+        <Tab h={"66px"} fontWeight={"bold"}>Futures</Tab>
+        <Tab h={"66px"} fontWeight={"bold"}>Options</Tab>
+        <Tab h={"66px"} fontWeight={"bold"}>User</Tab>
+        <Spacer/>
+      </TabList>
+      <TabPanels>
+        <TabPanel p={0}>
+          <Futures/>
+        </TabPanel>
+        <TabPanel p={0}>
+          <Options/>
+        </TabPanel>
+        <TabPanel p={0}>
+          <Users/>
+        </TabPanel>
+      </TabPanels>
+    </Tabs>
   )
 }
 
