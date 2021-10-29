f2c7066ac2fea26f0c5e0c1e4b80660295d2a569
tunogya <tunogya@qq.com>
2021-10-29 18:43:02 +0800

    update style

--- src/components/LineChart/index.tsx
+++ src/components/LineChart/index.tsx
@@ -31,7 +31,7 @@ const LineChart: FC<LineChartProps> = props => {
   };
 
   return (
-    <Stack height="616px" borderRadius={"20px"} boxShadow={"0 0 10px #E5E5E5"} p={["22px", "22px", "44px"]}>
+    <Stack borderRadius={"20px"} boxShadow={"0 0 10px #E5E5E5"} p={["22px", "22px", "44px"]} spacing={"8px"}>
       {props.title && (
         <Text fontSize={"18px"} color={"#878787"} fontFamily={"Montserrat"}>{props.title}</Text>
       )}
--- src/components/PieChart/index.tsx
+++ src/components/PieChart/index.tsx
@@ -22,7 +22,7 @@ const PieChart: FC<PieChartProps> = props => {
   };
 
   return (
-    <Stack  height="616px" borderRadius={"20px"} boxShadow={"0 0 10px #E5E5E5"} p={["22px", "22px", "44px"]}>
+    <Stack borderRadius={"20px"} boxShadow={"0 0 10px #E5E5E5"} p={["22px", "22px", "44px"]}>
       <Text fontSize={"18px"} color={"#878787"} fontFamily={"Montserrat"}>{props.title}</Text>
       <Pie {...config} />
     </Stack>
