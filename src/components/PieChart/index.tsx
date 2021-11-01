15170319a4360a18ae665df70b48cbc5c3ee6425
tunogya <tunogya@qq.com>
2021-11-01 14:58:26 +0800

    update color

--- src/components/PieChart/index.tsx
+++ src/components/PieChart/index.tsx
@@ -19,11 +19,12 @@ const PieChart: FC<PieChartProps> = props => {
       content: '{name} {percentage}',
     },
     interactions: [{type: 'pie-legend-active'}, {type: 'element-active'}],
+    color: ["#4FB08B", "#D7772C", "#F6E272", "#BEDE9F", "#5BCBEA"]
   };
 
   return (
     <Stack borderRadius={"20px"} boxShadow={"0 0 10px #E5E5E5"} p={["22px", "22px", "44px"]}>
-      <Text fontSize={"18px"} color={"#878787"} fontFamily={"Montserrat"}>{props.title}</Text>
+      <Text fontSize={"18px"} color={"#878787"} fontWeight={600} fontFamily={"Montserrat"}>{props.title}</Text>
       <Pie {...config} />
     </Stack>
   )
