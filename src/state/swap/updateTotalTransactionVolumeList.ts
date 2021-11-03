946f1c658be0de400ab469a424bf1f406b6c7464
tunogya <tunogya@qq.com>
2021-11-03 11:38:33 +0800

    update Total Transaction Volume

--- src/state/swap/updateTotalTransactionVolumeList.ts
+++ src/state/swap/updateTotalTransactionVolumeList.ts
@@ -1,6 +1,7 @@
 import {atomFamily, selectorFamily} from "recoil";
 import {TokenTxBlock} from "../app";
 import {swapTxListAtom} from "./index";
+import {web3} from "../../provider";
 
 export const totalTransactionVolumeListAtom = atomFamily({
   key: "swap-totalTransactionVolumeList::value",
@@ -14,8 +15,27 @@ export const totalTransactionVolumeListAtom = atomFamily({
 })
 
 const updateTotalTransactionVolumeList = (txList: TokenTxBlock[]) => {
-  console.log(txList)
-  return []
+  let totalTransactionVolumeListMap: {[index: string]: number} = {}
+  let transactionVolumeList: {day: string, value: number}[] = []
+
+  txList.forEach((block)=>{
+    if (block.tokenSymbol === "DCU"){
+      const date = new Date(Number(block.timeStamp)*1000).toJSON().substr(0, 10)
+      if (!totalTransactionVolumeListMap[date]){
+        totalTransactionVolumeListMap[date] = 0
+      }
+      totalTransactionVolumeListMap[date] += Number(web3.utils.fromWei(block.value))
+    }
+  })
+
+  Object.keys(totalTransactionVolumeListMap).forEach((key)=>{
+    transactionVolumeList.push({
+      day: key,
+      value: totalTransactionVolumeListMap[key],
+    })
+  })
+
+  return transactionVolumeList
 }
 
 export default updateTotalTransactionVolumeList
