afc7fc1a885472c22365f2e92ae0786e20cedce5
tunogya <tunogya@qq.com>
2021-11-05 21:02:47 +0800

    update date

--- src/utils/fillAllDayToInitMap.test.ts
+++ src/utils/fillAllDayToInitMap.test.ts
@@ -4,10 +4,9 @@ describe('test fillAllDayToInitMap', () => {
   it('fill AllDay Init Object Map', () => {
     const now = new Date().getTime()
     const begin = new Date("2021.10.1 8:0:0").getTime()
-    console.log(new Date(1633046400000))
-    // let map = {}
-    // map = fillAllDayToInitMap(map, now, begin, "number")
-    // console.log(map)
+    let map = {}
+    map = fillAllDayToInitMap(map, now, begin, "set")
+    console.log(map)
     // @ts-ignore
   })
 })
