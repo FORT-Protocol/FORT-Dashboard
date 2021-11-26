e03793d403a20d7a533c38a1f27bd9968831718a
tunogya <tunogya@qq.com>
2021-11-26 15:28:42 +0800

    fixed options

--- src/pages/Options/index.tsx
+++ src/pages/Options/index.tsx
@@ -2,17 +2,6 @@ import {SimpleGrid, Stack} from "@chakra-ui/react";
 import Norm from "../../components/Norm";
 import LineChart from "../../components/LineChart";
 import PieChart from "../../components/PieChart";
-import {useRecoilValue} from "recoil";
-import {totalTxVolumeAtom} from "../../state/options/updateTotalOpenPosition";
-import {longShortDistributionAtom} from "../../state/options/updateLongShortDistribution";
-import {totalTxVolumeListAtom} from "../../state/options/updateOpenPosition";
-import {distributionOfExerciseTimespanAtom} from "../../state/options/updateDistributionOfExerciseTimespan";
-// import {
-//   OpenInterestAtom
-// } from "../../state/options/updateOpenInterest";
-import {statusAtom} from "../../hooks/useFetchOptionsTxList";
-import {PROCESSING} from "../../constant/status";
-import {optionsCumulativeNumberOfTransactionAtom} from "../../state/options/updateCumulativeNumberOfTransaction";
 import {useEffect, useState} from "react";
 
 const Options = () => {
@@ -20,9 +9,8 @@ const Options = () => {
   const [currentBullishQty, setCurrentBullishQty] = useState("-")
   const [currentBearishQty, setCurrentBearishQty] = useState("-")
   const [distributionOfLongShort, setDistributionOfLongShort] = useState<{value: number, type: string}[]>([])
-  const totalTxVolumeList = useState("-")
-  const distributionOfExerciseTimespan = useRecoilValue(distributionOfExerciseTimespanAtom({}))
-  // const totalTradingVolumeList = useRecoilValue(OpenInterestAtom({}))
+  const [totalTxVolumeList, setTotalTxVolumeList] = useState<{ day: string, value: number, category: string }[]>([])
+  const [distributionOfExerciseTimespan, setDistributionOfExerciseTimespan] = useState<{value: number, type: string}[]>([])
   const [cumluativeNumberOfTransaction, setCumluativeNumberOfTransaction] = useState("-")
 
   useEffect(()=>{
@@ -46,27 +34,31 @@ const Options = () => {
       .then((res) => res.json())
       .then((json) => setCumluativeNumberOfTransaction(json["value"])
       )
-    fetch("https://api.hedge.red/api/options/dirDist")
+    fetch("http://192.168.2.52:8080/api/options/dirDist/total")
       .then((res) => res.json())
       .then((json) => setDistributionOfLongShort(json["value"])
       )
-
-
-
+    fetch("http://192.168.2.52:8080/api/options/openedQtyOfDaily/total")
+      .then((res) => res.json())
+      .then((json) => setTotalTxVolumeList(json["value"])
+      )
+    fetch("http://192.168.2.52:8080/api/options/strikePriceDist")
+      .then((res) => res.json())
+      .then((json) => setDistributionOfExerciseTimespan(json["value"])
+      )
   }
 
 
   return (
     <Stack spacing={["22px", "22px", "44px"]} p={["22px", "22px", "44px"]}>
       <SimpleGrid columns={[1, 1, 2, 2, 4]} spacing={["22px", "22px", "44px"]}>
-        <Norm value={totalTxVolume} desc={"Total Open Position"} color={"#C7A072"}/>
+        <Norm value={totalTxVolume} desc={"Total Open Position(DCU)"} color={"#C7A072"}/>
         <Norm value={currentBullishQty} desc={"Current Call Option Positions"} color={"#00B388"}/>
         <Norm value={currentBearishQty} desc={"Current Short Option Positions"} color={"#F23A12"}/>
         <Norm value={cumluativeNumberOfTransaction} desc={"Cumulative Number of Transaction"} color={"#E57200"} />
       </SimpleGrid>
       <SimpleGrid columns={1} spacing={["22px", "22px", "44px"]}>
-        {/*<LineChart title={"Open Position"} data={totalTxVolumeList} suffix={"DCU"}/>*/}
-        {/*<LineChart title={"Open Interest"} data={totalTradingVolumeList} noTotal/>*/}
+        <LineChart title={"Open Position"} data={totalTxVolumeList} suffix={"DCU"}/>
       </SimpleGrid>
       <SimpleGrid columns={[1, 1, 1, 2]} spacing={["22px", "22px", "44px"]}>
         <PieChart title={"Long-Short Distribution"} data={distributionOfLongShort}/>
